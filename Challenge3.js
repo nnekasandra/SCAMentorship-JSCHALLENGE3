2712ea57ac490eeb50971c43c2a17844f0413a9e
Amira Adediran <amira@Amiras-MacBook-Pro.local>
2020-10-14 18:33:46 +0100

    update file

--- Challenge3.js
+++ Challenge3.js
@@ -1,12 +1,11 @@
-<!-- Hello everyone!
+/* Hello everyone!
 This is your third JavaScript challenge.
-
 
 CHALLENGE!
 
 Write a function called reverseNumber that reverses a number.
 
-Input Example: 
+nput Example: 
 12345
 555
 
@@ -14,6 +13,5 @@ Output Example:
 54321
 555
 
-Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :)
+Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :) */
 
--->
