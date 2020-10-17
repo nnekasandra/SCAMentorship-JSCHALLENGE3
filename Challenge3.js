bf0abbbf9da42ac9bdd818c2bab783ddf3e1cd5c
njoku nneka sandra <nnekasandra2016@gmail.com>
2020-10-17 09:16:31 +0100

    initial commit

--- Challenge3.js
+++ Challenge3.js
@@ -15,3 +15,16 @@ Output Example:
 
 Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :) */
 
+function reverseNumber (num){
+  if (typeof num == 'number') {
+    num = num.toString();
+  }
+  var reversed = '';
+  for (var i = num.length-1; i>=0; i--){
+    var char = num [i];
+    reversed = reversed + char;
+  }
+  return reversed
+};
+console.log(reverseNumber(555667));
+console.log(reverseNumber(123667));
